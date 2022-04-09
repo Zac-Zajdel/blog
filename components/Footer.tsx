5b0f772c2b36d3a969cef1def88dfefdc7806f76
Zac-Zajdel <zaczajdel213@gmail.com>
2022-04-09 02:58:15 -0400

    More styling changes and converting more files to typescript

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ import SocialIcon from '@/components/social-icons'
 export default function Footer() {
   return (
     <footer>
-      <div className="mt-16 flex flex-col items-center">
+      <div className="mt-16 flex flex-col items-center border-t-2 border-gray-200 pt-6 dark:border-gray-700">
         <div className="mb-3 flex space-x-4">
           <SocialIcon kind="mail" href={`mailto:${siteMetadata.email}`} size={6} />
           <SocialIcon kind="github" href={siteMetadata.github} size={6} />
