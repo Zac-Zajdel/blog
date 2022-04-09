88160dba848d04650948cfdf36bc484c46068182
Zac-Zajdel <zaczajdel213@gmail.com>
2022-04-09 19:21:29 -0400

    Lot of progress on my longest blog post so far

--- components/MDXComponents.js
+++ components/MDXComponents.js
@@ -7,6 +7,14 @@ import TOCInline from './TOCInline'
 import Pre from './Pre'
 import { BlogNewsletterForm } from './NewsletterForm'
 
+/**
+ * We have the following customizations:
+ * 1. Using Next images
+ * 2. Table of contents
+ * 3. Uses Next links
+ * 4. Generates Blog underneath each post
+ * 5. Wraps each around our default layout
+ */
 export const MDXComponents = {
   Image,
   TOCInline,
